999e55107f792c9fc7f673d6c3ea78bd993b1ed2
jafar <144897803+Jafar777@users.noreply.github.com>
2024-07-29 14:32:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://Jafar777.github.io/React-portfolio/",
+  base:"https://github.com/Jafar777/React-portfolio",
   plugins: [react()],
 })
