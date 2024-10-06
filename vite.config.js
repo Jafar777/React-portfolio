706c4cee5a09430d19bc0680b4bfa0e1df890a5f
jafar <144897803+Jafar777@users.noreply.github.com>
2024-10-06 09:42:44 +0300

    viteConf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://jafaralsoubeni.com/",
+  base: "/",  // Use "/" to indicate the root of your custom domain
   plugins: [react()],
 })
